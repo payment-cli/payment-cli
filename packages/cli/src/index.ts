41ece14eccbffce1dc2e7e76d32a3f086b012783
Diego García <dgarciavallejos@gmail.com>
2024-12-04 23:29:34 -0300

    feat: enhance CLI command structure and improve program metadata

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,21 +1,23 @@
 import { Command } from "commander";
 import dotenv from "dotenv";
+
+import { add } from "@/commands/add";
 import { getPackageInfo } from "@/utils";
-import ora from "ora";
-import { add } from "./commands/add";
 
 dotenv.config();
-export const spinner = ora();
 
 export const main = async () => {
   const packageInfo = getPackageInfo();
   const program = new Command();
 
-  program.version(
-    packageInfo.version || "1.0.0",
-    "-v, --version",
-    "display the version number "
-  );
+  program
+    .name(packageInfo.name || "cli")
+    .description(packageInfo.description || "A CLI for managing packages")
+    .version(
+      packageInfo.version || "1.0.0",
+      "-v, --version",
+      "display the version number "
+    );
 
   program.addCommand(add);
 
