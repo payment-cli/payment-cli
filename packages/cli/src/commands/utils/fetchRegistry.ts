86d2dda2d32a780a2d2a8fa22b9aa6b0b2de6d80
Diego García <dgarciavallejos@gmail.com>
2024-12-04 23:27:16 -0300

    refactor: improve utilities and documentation

--- packages/cli/src/commands/utils/fetchRegistry.ts
+++ packages/cli/src/commands/utils/fetchRegistry.ts
@@ -1,17 +1,23 @@
 import { handleErrors } from "@/utils";
+import { RegistryListType } from "@/commands/types";
 
-export const fetchRegistry = async (paths: string[]) => {
+/**
+ * Fetches registry data from the specified paths.
+ *
+ * @param {string[]} paths - An array of paths to fetch the registry data from.
+ * @returns {Promise<RegistryListType[]>} A promise that resolves to an array of registry data.
+ * @throws Will throw an error if the fetch operation fails.
+ */
+export default async function fetchRegistry(paths: string[]): Promise<RegistryListType> {
   try {
-    const results = await Promise.all(
-      paths.map(async (path) => {
+    return await Promise.all(
+      paths.map(async (path) => {        
         const response = await fetch(
           `${process.env.REGISTRY_URL ?? "https://docs.paymentcli.xyz/registry"}/${path}`
-        );
+        );        
         return await response.json();
       })
     );
-
-    return results;
   } catch (error) {
     handleErrors("Failed to connect to the components library.");
     process.exit(0);
