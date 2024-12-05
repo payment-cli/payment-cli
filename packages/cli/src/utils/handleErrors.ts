8394e15806934223852de2908f9367eff0bbf948
Diego García <dgarciavallejos@gmail.com>
2024-12-05 00:04:35 -0300

    fix: update error handling to exit with non-zero status on errors

--- packages/cli/src/utils/handleErrors.ts
+++ packages/cli/src/utils/handleErrors.ts
@@ -12,14 +12,14 @@ import { logger } from "@/utils";
 export default function handleErrors(error: unknown) {
   if (typeof error === "string") {
     logger.error(error);
-    process.exit(0);
+    process.exit(1);
   }
 
   if (error instanceof Error) {
     logger.error(error.message);
-    process.exit(0);
+    process.exit(1);
   }
 
   logger.error("Something went wrong. Please try again.");
-  process.exit(0);
+  process.exit(1);
 };
